ba7331178e8069242ccc0cce2590f89719c4cf56
Debraj-Das <debrajdas.gayatri@gmail.com>
2023-03-09 10:03:01 +0530

    Add the matrial files in local reprojectory

---
+++ Heap_BT.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void solve();
+
+int main()
+{ 
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int t = 1;
+    scanf("%d",&t);
+    while(t--) solve();
+    
+    return 0;
+}
+
+void solve()
+{
+    int n ;
+    scanf("%d",&n);
+    int arr[n];
+    for (size_t i = 0; i < n; i++)
+    {
+        
+    }
+    
+    return ;
+}
